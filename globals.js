c77583c790533527c7e5726d89432de849d6d0ca
Vinski <vinski.andrade@visy.fi>
2025-08-19 19:26:24 +0300

    let and const applied to globals

--- globals.js
+++ globals.js
@@ -1,12 +1,11 @@
-var vector,
-  charStyle = document.getElementById('char01').style,
-  charHorSpeed = .5, // movements.js
-  charVerSpeed = 1, // movements.js
+let vector,
   action1 = false,
   action2 = false,
   walls = [],
-  charHeight = 6,
-  charWidth = 3,
-  gapBorderTolerance = 2,
   wallsAmount,
   gameOn = true;
+  
+const charStyle = document.getElementById('char01').style,
+  charHeight = 6,
+  charWidth = 3,
+  gapBorderTolerance = 2;
